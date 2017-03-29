f1307d5e1290c39d2545ceee44292a8afc1ee1d1
eyraniel <tat_tut@mail.ru>
2017-03-29 21:32:52 +0300

    added about fragment

--- app/src/main/java/ru/tinkoff/shishkova/tfsproject/AboutFragment.java
+++ app/src/main/java/ru/tinkoff/shishkova/tfsproject/AboutFragment.java
@@ -2,15 +2,11 @@ package ru.tinkoff.shishkova.tfsproject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
-
-    private String desc = "Project for Tinkoff Fintech School\nAuthor:Tatyana Shishkova";
 
     public static AboutFragment newInstance() {
         AboutFragment fragment = new AboutFragment();
@@ -25,8 +21,6 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-        TextView textViewTitle = (TextView) view.findViewById(R.id.text_view_about);
-        textViewTitle.setText(desc);
         return view;
     }
 }
--- app/src/main/java/ru/tinkoff/shishkova/tfsproject/NavigationActivity.java
+++ app/src/main/java/ru/tinkoff/shishkova/tfsproject/NavigationActivity.java
@@ -1,8 +1,6 @@
 package ru.tinkoff.shishkova.tfsproject;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
